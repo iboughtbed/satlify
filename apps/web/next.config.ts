4330c61167bb2a81aa69318072f8a60472bc7a6f
iboughtbed <testpythontosanzhar@gmail.com>
2025-05-12 18:14:43 +0500

    fix: next config to ts

---
+++ apps/web/next.config.ts
@@ -0,0 +1,44 @@
+import type { NextConfig } from "next";
+// import createNextIntlPlugin from "next-intl/plugin";
+
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+import "./src/env";
+
+const config: NextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+      },
+    ],
+  },
+  compiler: {
+    removeConsole:
+      process.env.NODE_ENV === "production"
+        ? {
+            exclude: ["error"],
+          }
+        : undefined,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  devIndicators: false,
+};
+
+// const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
+
+// export default withNextIntl(config);
+export default config;
