5df8aea33f3aa33701e6e1b2966b4cf9b87f4b53
iboughtbed <testpythontosanzhar@gmail.com>
2025-05-12 18:56:53 +0500

    fix: temporarily remove social providers for auth

--- apps/web/src/lib/auth.ts
+++ apps/web/src/lib/auth.ts
@@ -1,13 +1,18 @@
 import { cache } from "react";
+// import { redirect } from "next/navigation";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { username } from "better-auth/plugins";
+// import { eq } from "drizzle-orm";
 
 import { db } from "@/lib/db";
 import * as schema from "@/lib/db/schema";
-import { env } from "@/env";
+// import { env } from "@/env";
 
 export const auth = betterAuth({
+  emailAndPassword: {
+    enabled: true,
+  },
   database: drizzleAdapter(db, {
     provider: "pg",
     schema: {
@@ -25,13 +30,12 @@ export const auth = betterAuth({
     updateAge: 60 * 60 * 24, // 1 day (every 1 day the session expiration is updated)
   },
   socialProviders: {
-    github: {
-      clientId: env.GITHUB_CLIENT_ID,
-      clientSecret: env.GITHUB_CLIENT_SECRET,
-    },
+    // github: {
+    //   clientId: process.env.GITHUB_CLIENT_ID!,
+    //   clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    // },
   },
-  trustedOrigins: process.env.BETTER_AUTH_TRUSTED_ORIGINS?.split(",") ?? [],
+  trustedOrigins: process.env.BETTER_AUTH_TRUSTED_ORIGIN?.split(",") ?? [],
 });
 
-// cache per request
 export const getSession = cache(auth.api.getSession);
